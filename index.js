f4a22398ec07571b82a95632f52f523768732425
Jito Chadha <jitochadha@gmail.com>
2021-03-03 14:39:37 -0800

    initial files

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 const inquirer = require("inquirer");
 
 inquirer
-    .prompt();
+    .prompt([{
+        type: 'text',
+        message: 'Email?',
+        name: 'email',
+    }, {
+        type: 'list',
+        message: 'Which potion would you like to use?',
+        name: 'action',
+        choices
+    }]).then(answers => {
+
+    });
+
+
+inquirer.prompt({
+}).then(({ action }) => {
+    const potionDetails = action.split(': ');
+
+    this.player.usePotion(potionDetails[0] - 1);
+    console.log(`You used a ${potionDetails[1]} potion.`);
+
+    return this.checkEndOfBattle();
+});
